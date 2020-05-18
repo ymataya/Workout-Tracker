63fcf30c7a22fb85d5ce005ef6873bad0c572c09
Yna Mataya <ylmataya@yahoo.com>
2020-05-18 15:04:16 -0500

    none

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {useNewUrlParser: true});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workouts";
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 
 // HTML ROUTES //
 app.get("/", (req, res) => {
