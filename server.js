45054c904d797c669a40065e0e280ddacaece073
Yna Mataya <ylmataya@yahoo.com>
2020-05-20 09:51:47 -0500

    a

--- server.js
+++ server.js
@@ -12,9 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workouts";
 //USE FOR LOCAL HOST
-//|| "mongodb://localhost/workouts";
 console.log(MONGODB_URI)
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 
